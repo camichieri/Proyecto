1372f0b3b4b78ae45fee994da5fb14a3690c9bac
Cami.ch <camichieri@gmail.com>
2024-08-03 22:33:52 -0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import BottomNavbar from './components/BottomNavbar.jsx';
-import HomePage from './pages/HomePage.jsx';
 import ContactsPage from './pages/ContactsPage.jsx';
 import ChatsPage from './pages/ChatsPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
@@ -13,7 +12,6 @@ function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/chats" element={<ContactsPage />} />
         <Route path="/chats/:contactId" element={<ChatsPage />} />
         <Route path="/profile/:contactId" element={<ProfilePage />} />
